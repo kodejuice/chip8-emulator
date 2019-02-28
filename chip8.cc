ee1afb8da25bd5326c2fb5f4a7b9dc82610aaa02
kodejuice <kodejuice@gmail.com>
2019-02-28 20:41:14 +0100

    bug fixes, alot

--- chip8.cc
+++ chip8.cc
@@ -13,9 +13,6 @@
 
 mt19937 rnd{};
 
-// TODO: Implement emulator loop
-// TODO: Study SDL
-// TODO: Beep Sound
 
 /**
  * Load ROM into memory
@@ -38,8 +35,8 @@ bool Chip8::load_program(string file) {
 	fread(rom, sizeof(byte), program_size, f);
 
 	// load program from buffer into memory
-	for (int i=PC, j=0; i < int(PC + program_size); ++i) {
-		memory[i] = (byte) rom[j++];
+	for (int i=PC, j=0; i < int(PC + program_size); ++i, ++j) {
+		memory[i] = rom[j];
 	}
 
 	free(rom);
@@ -57,43 +54,41 @@ bool Chip8::load_program(string file) {
  * See chip8 instruction set at http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1
  */
 void Chip8::emulate_op() {
-	#define not_handled(op)\
-		printf("\nUnrecognized instruction: %04x\n", op); exit(2);
+	#define not_handled(m,l)\
+		printf("\nUnrecognized instruction: %04x %04x\n", m,l); exit(2);
 
-	byte *op = &memory[PC];
-	byte msb = *op, lsb = *(op + 1);
 	int tmp;
-
-	// increment program counter
-	//  point to next instruction
-	PC += 2;
+	int opcode = (memory[PC] << 8) | memory[PC+1];
+	int msb = opcode>>8, lsb = opcode&0xff;
 
 	// Get bit-fields from instruction/opcode
-	const byte x = msb&0xf,					// 0x_x__
-			y = lsb >> 4,					// 0x__y_
-			kk = lsb,						// 0x__kk
-			n = kk&0xf,						// 0x___n
-			nnn = ((msb&0xf)<<8) | lsb; 	// 0x_nnn
+	int u   = (opcode>>12) & 0xF,
+		x   = (opcode>>8) & 0xF,
+		y   = (opcode>>4) & 0xF,
+		kk  = (opcode) & 0xFF,
+		n   = (opcode) & 0xF,
+		nnn = (opcode) & 0xFFF;
 
-	// alliases for registers
+	// alliases for common registers
 	byte &Vx = V[x], &Vy = V[y], &VF = V[0xF];
 
 	switch (msb >> 4) {
 		// 0x0e-
 		case 0x0: {
-			switch (lsb) {
-				// 0x0e0
-				case 0xe0: // clr
+			switch (lsb&0xf) {
+				// 0x00e0
+				case 0x0: // clr
 					memset(screen, 0, 2048);
+					PC += 2;
 					break;
 
-				// 0x0ee
-				case 0xee: // ret
-					PC = stack[--sp];
+				// 0x00ee
+				case 0xe: // ret
+					PC = stack[--sp] + 2;
 					break;
 
 				default:
-					not_handled(lsb);
+					not_handled(msb, lsb);
 			}
 		}
 		break;
@@ -112,26 +107,31 @@ void Chip8::emulate_op() {
 		// 03xkk
 		case 0x3: // jeq Vx, Vkk
 			if (Vx == kk) PC += 2;
+			PC += 2;
 			break;
 
 		// 0x4xkk
 		case 0x4: //jneq Vx, Vkk
 			if (Vx != kk) PC += 2;
+			PC += 2;
 			break;
 
 		// 0x5xy0
 		case 0x5: // jeqr Vx, Vy
 			if (Vx == Vy) PC += 2;
+			PC += 2;
 			break;
 
 		// 0x6xkk
 		case 0x6: // mov Vx, kk
 			Vx = kk;
+			PC += 2;
 			break;
 
 		// 0x7xkk
-		case 0x7: // add Vx, Vkk
-			Vx += V[kk];
+		case 0x7: // add Vx, kk
+            Vx += kk;
+			PC += 2;
 			break;
 
 		// 0x8xyn
@@ -140,21 +140,25 @@ void Chip8::emulate_op() {
 				// 0x8xy0
 				case 0x0: // mov Vx, Vy
 					Vx = Vy;
+					PC += 2;
 					break;
 
 				// 0x8xy1
 				case 0x1: // or Vx, Vy
 					Vx |= Vy;
+					PC += 2;
 					break;
 
 				// 0x8xy2
 				case 0x2: // and Vx, Vy
-					Vx = Vx & Vy;
+					Vx &= Vy;
+					PC += 2;
 					break;
 
 				// 0x8xy3
 				case 0x3: // xor Vx, Vy
-					Vx = Vx ^ Vy;
+					Vx ^= Vy;
+					PC += 2;
 					break;
 
 				// 0x8xy4
@@ -162,34 +166,39 @@ void Chip8::emulate_op() {
 					tmp = Vx + Vy;
 					VF = (tmp > 0xff);
 					Vx += Vy;
+					PC += 2;
 					break;
 
 				// 0x8xy5
 				case 0x5: // sub Vx, Vy
 					VF = Vx > Vy;
 					Vx -= Vy;
+					PC += 2;
 					break;
 
 				// 0x8xy6
 				case 0x6: // shr Vx
 					VF = Vx&1;
 					Vx >>= 1;
+					PC += 2;
 					break;
 
 				// 0x8xy7
 				case 0x7: // subb Vy, Vx
 					VF = Vy > Vx;
 					Vy -= Vx;
+					PC += 2;
 					break;
 
 				// 0x8ye
 				case 0xe: // shl Vx
 					VF = Vx >> 7;
 					Vx <<= 1;
+					PC += 2;
 					break;
 
 				default:
-					not_handled(lsb);
+					not_handled(msb, lsb);
 			}
 		}
 		break;
@@ -197,11 +206,13 @@ void Chip8::emulate_op() {
 		// 9xy0
 		case 0x9: // jneqr Vx, Vy
 			if (Vx != Vy) PC += 2;
+			PC += 2;
 			break;
 
 		// Annn
 		case 0xA: // mov I, nnn
 			I = nnn;
+			PC += 2;
 			break;
 
 		// 0xBnnn
@@ -212,6 +223,7 @@ void Chip8::emulate_op() {
 		// 0xCxkk
 		case 0xC: // rnd Vx, kk
 			Vx = uniform_int_distribution<>(0, 255)(rnd) & kk;
+			PC += 2;
 			break;
 
 		// 0xDxyn
@@ -240,9 +252,9 @@ void Chip8::emulate_op() {
 						pixel ^= 1;
 					}
 				}
-
 				++row_pixels;
 			}
+			PC += 2;
 		}
 		break;
 
@@ -252,11 +264,13 @@ void Chip8::emulate_op() {
 				// 0xE9E
 				case 0x9E: // jkey Vx
 					if (key_pressed[Vx]) PC += 2;
+					PC += 2;
 					break;
 
 				// 0xA1
 				case 0xA1: // jnkey Vx
 					if (!key_pressed[Vx]) PC += 2;
+					PC += 2;
 					break;
 			}
 		}
@@ -268,31 +282,38 @@ void Chip8::emulate_op() {
 				// 0xFx07
 				case 0x07: // getdelay Vx
 					Vx = DT;
+					PC += 2;
 					break;
 
 				// 0xFx0A
 				case 0x0A: // waitkey Vx
 					awaitingKey = 0x80 | x;
+					PC += 2;
 					break;
 
 				// 0xFx15
 				case 0x15: // setdelay Vx
 					DT = Vx;
+					PC += 2;
 					break;
 
 				// 0xFx18
 				case 0x18: // setsound Vx
 					ST = Vx;
+					PC += 2;
 					break;
 
 				// 0xFx1E
 				case 0x1E: // mov I, Vx
 					I += Vx;
+					VF = (I+Vx > 0xff);
+					PC += 2;
 					break;
 
 				// 0xFx29
 				case 0x29: // spritei I, Vx
 					I = 5 * Vx;
+					PC += 2;
 					break;
 
 				// 0xFx33
@@ -304,30 +325,42 @@ void Chip8::emulate_op() {
 					a = tmp;
 
 					memory[I] = a, memory[I+1] = b, memory[I+2] = c;
+					PC += 2;
 					break;
 
 				// 0xFx55
-				case 0x55: // mov [I], V0-VF
+				case 0x55: { // mov [I], V0-VF
+					byte *memv = &memory[I];
+
 					for (int p = 0; p <= x; ++p)
-						memory[I+p] = V[p];
-					break;
+						*(memv++) = V[p];
+
+					I += x+1, PC += 2;
+				}
+				break;
 
 				// 0xFx65
-				case 0x65: // mov V0-VF, [I]
+				case 0x65: { // mov V0-VF, [I]
+					byte *memv = &memory[I];
+
 					for (int p = 0; p <= x; ++p)
-						V[p] = memory[I+p];
+						V[p] = *(memv++);
+
+					I += x+1, PC += 2;
 					break;
+				}
 
 				default:
-					not_handled(lsb);
+					not_handled(msb, lsb);
 			}
 			break;
 		}
 
 		default:
-			not_handled(msb);
+			not_handled(msb, lsb);
 	}
 	#undef not_handled
+
 
 	// Time & Sound
 
